b5ff01143fe8b1c5af76706dcc5ed9adfacb8686
BaoHaoYu <a450215151@outlook.com>
2019-12-05 14:47:48 +0800

    del: storybook的jest

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,12 +1,5 @@
-import { withTests } from '@storybook/addon-jest'
-import { addDecorator, configure } from '@storybook/react'
+import { configure } from '@storybook/react'
 
-addDecorator(
-  withTests({
-    results: require('../.jest-test-results.json'),
-    filesExt: '((\\.specs?)|(\\.tests?))?(\\.tsx)?$',
-  }) as any
-)
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.tsx$/)
 
